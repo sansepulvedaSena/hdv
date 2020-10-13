a0f666cc74271dca8e91db2aabe6ec39a72c1904
Santiago <ssepulveda532@misena.edu.co>
2020-10-12 21:03:38 -0500

    Hoja de vida

--- assets/config.js
+++ assets/config.js
@@ -9,16 +9,16 @@ const persona = {
 	telefono: "+57 301 639 5335",
 	perfil: "Estudiante de Tecnología en Análisis y Desarrollo de Sistemas de Información en el SENA. 7mo semestre suspendido de Comunicación Audiovisual en el Politécnico Colombiano Jaime Isaza Cadavid",
 	habilidades: {
-		comunicativas:['Asertivo, ','Persuasivo, ','Actitud Positiva, ','Tolerancia, ','Respeto, ','Diálogo Abierto, ','Orden, ','Apoyo'],
-		gestion: ['Liderazgo, ','Evaluación de Proyectos, ','Responsabilidad, ','Estrategias de Gestión'],
-		tecnicas: ['Análisis de datos, ','Python (WEB / ETL), ','FullStack MEAN, ','Javascript, ','Bases de Datos SQL, ','MySQL - PlSQL, ', 'MongoDB - NoSQL', 'Office']
+		comunicativas:['','Persuasivo, ','Actitud Positiva, ','Tolerancia, ','Respeto, ','Diálogo Abierto, ','Orden, ','Apoyo','Capacidad de escucha'],
+		gestion: ['Liderazgo, ','Responsabilidad','Puntualidad'],
+		tecnicas: ['Manejo básico NetBeans','Manejo medio de Adobe Premiere y Sony Vegas Pro']
 	},
-	hobbies: ['Escuchar Música, ', 'Ver Peliculas, ', 'Tocar Guitarra, ', 'Video-Juegos']
+	hobbies: ['Escuchar Música, ', 'Ver Peliculas, ', 'Tocar Guitarra y batería, ', 'Video-Juegos','Compartir tiempo en amistad']
 }
 
 // Dejar intácto a partir de aquí.
 // Constantes SENA:
-const banner = "https://3.bp.blogspot.com/-TkVKyVxdxgQ/U2kGFcBl3LI/AAAAAAAABVo/zrDM3ittaz0/s1600/Banner+Soy+Cultura+SENA+-+CTMA.png";
+const banner = "https://scontent-bog1-1.xx.fbcdn.net/v/t1.0-9/93939364_3221922414506022_5702114881294041088_o.jpg?_nc_cat=108&_nc_sid=e3f864&_nc_eui2=AeFy_XvrsWjaofLxmK7HtVwxUMVJ6F7haCVQxUnoXuFoJasJZ5cH1olEv2ges9Ryr-Ykt1ir_UIPin41_jS-e9v5&_nc_ohc=6hPelIxyRtgAX8Cbe-R&_nc_ht=scontent-bog1-1.xx&oh=ab66b3f6ce83164c853e2d730441255d&oe=5FA91377";
 // Mostramos el objeto persona en la consola:
 console.log(persona);
 
